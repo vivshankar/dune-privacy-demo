18ef3425d47be11663abee889b21634571cb987d
Vivek Shankar <shankarv@sg.ibm.com>
2021-06-21 17:59:16 +0800

    fix: increased session timeout to 30 mins

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.use(session({
     secret: 'supersecret',
     resave: false,
     saveUninitialized: true,
-    cookie: { path: '/', maxAge: 120 * 1000, secure: false }
+    cookie: { path: '/', maxAge: 30 * 60 * 1000, secure: false }
 }))
 
 // define routes
